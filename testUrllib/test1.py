4ea0c2b07046272d6d06448c0b874fb7b2e03bf8
Fish <cqkxxc>
2021-08-18 21:36:08 +0800

    urllib测试，user-agent伪装

--- testUrllib/test1.py
+++ testUrllib/test1.py
@@ -4,6 +4,48 @@
 # @Software : PyCharm
 
 import urllib.request
+import urllib.parse
 
-response = urllib.request.urlopen("http://www.baidu.com")
-print(response.read().decode('utf-8'))
+# 获取一个GET请求
+# response = urllib.request.urlopen("http://www.baidu.com")
+# print(response.read().decode('utf-8'))  #对获取的网页源码进行UTF-8解码
+
+
+# 获取一个POST请求
+# import urllib.parse
+# data =bytes(urllib.parse.urlencode({"hello":"world"}),encoding="utf-8")
+# response = urllib.request.urlopen("http://httpbin.org/post",data=data)
+# print(response.read().decode("utf-8"))
+
+# 超时处理
+# try:
+#     response = urllib.request.urlopen("http://httpbin.org/get",timeout=0.01)
+#     print(response.read().decode("utf-8"))
+# except urllib.error.URLError as e:
+#     print("timeout!")
+
+
+# response = urllib.request.urlopen("http://www.baidu.com")
+# print(response.status)
+# # print(response.getheaders())
+# print(response.getheader("Server"))
+
+
+# url = "https://www.douban.com"
+# url = "http://httpbin.org/post"
+# headers = {
+#     "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.73"
+# }
+# data = bytes(urllib.parse.urlencode({'name': 'haha'}), encoding="utf-8")
+# req = urllib.request.Request(url=url, data=data, headers=headers, method="POST")
+# response = urllib.request.urlopen(req)
+# print(response.read().decode("utf-8"))
+
+url = "https://www.douban.com"
+headers = {
+    "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.73"
+}
+req = urllib.request.Request(url=url,headers=headers)
+response = urllib.request.urlopen(req)
+print(response.read().decode("utf-8"))
+
